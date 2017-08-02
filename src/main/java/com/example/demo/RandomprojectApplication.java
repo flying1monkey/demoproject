10bb30a305dfb731faf469a13cea31ea33e0bb40
Jesse <jesseberliner@gmail.com>
2017-08-02 11:37:19 -0400

    This is my first commit...never type such a message.

---
+++ src/main/java/com/example/demo/RandomprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomprojectApplication.class, args);
+	}
+}
